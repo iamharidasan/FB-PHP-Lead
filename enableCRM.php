668061e677c65749d70f8f35e510739a8819a079
Haridasan Shanmugam <haridasan@socialbeat.in>
2020-03-19 15:32:43 +0530

    Real escape String added for Form Names

--- enableCRM.php
+++ enableCRM.php
@@ -19,11 +19,12 @@ if(isset($_POST['formid'])){
     $formdata = json_decode($data,true);
     $selectQuery = "SELECT * FROM `".TABLE_PREFIX."forms` WHERE `formID` = '".$_POST['formid']."'";
     $result = $db->query($selectQuery);
+    $formname = $db->real_escape_string($formdata['name']);
     if($result->num_rows==0){
-        $insertQuery = "INSERT INTO `".TABLE_PREFIX."forms` (`formID`,`formName`,`accessToken`,`projectName`) VALUES ('".$_POST['formid']."','".$formdata['name']."','".$_POST['pageToken']."','".$_POST['projectName']."')";
+        $insertQuery = "INSERT INTO `".TABLE_PREFIX."forms` (`formID`,`formName`,`accessToken`,`projectName`) VALUES ('".$_POST['formid']."','".$formname."','".$_POST['pageToken']."','".$_POST['projectName']."')";
         $db->query($insertQuery);
     }else{
-        $updateQuery = "UPDATE `".TABLE_PREFIX."forms` SET `formName` = '".$formdata['name']."', `accessToken` = '".$_POST['pageToken']."', `projectName` = '".$_POST['projectName']."' WHERE `formID` = '".$_POST['formid']."')";
+        $updateQuery = "UPDATE `".TABLE_PREFIX."forms` SET `formName` = '".$formname."', `accessToken` = '".$_POST['pageToken']."', `projectName` = '".$_POST['projectName']."' WHERE `formID` = '".$_POST['formid']."')";
         $db->query($updateQuery);
     }
     echo "{status:'Success'}";
